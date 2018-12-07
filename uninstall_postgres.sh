bea974c46d4542488ae2c6f0caaae38183332708
dbc2000 <38680220+dbc2000@users.noreply.github.com>
2018-12-07 15:03:07 +0100

    Create uninstall_postgres.sh

---
+++ uninstall_postgres.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo "Veuillez saisir la version de PostgreSQL a desinstallée (9.4/9.5/9.6/9.6/10/11) :"
+read pg_version
+echo $pg_version
+
+if [ $pg_version = "9.4" ]
+then
+yum remove postgresql94 postgresql94-server postgresql94-contrib postgresql94-libs
+rm -rf /var/lib/pgsql/9.4
+rm -rf /usr/pgsql-9.4
+fi
+
+if [ $pg_version = "9.5" ]
+then
+yum remove postgresql95 postgresql95-server postgresql95-contrib postgresql95-libs
+rm -rf /var/lib/pgsql/9.5
+rm -rf /usr/pgsql-9.5
+fi
+
+if [ $pg_version = "9.6" ]
+then
+yum remove postgresql96 postgresql96-server postgresql96-contrib postgresql96-libs
+rm -rf /var/lib/pgsql/9.6
+rm -rf /usr/pgsql-9.6
+fi
+
+if [ $pg_version = "10" ]
+then
+yum remove postgresql10 postgresql10-server postgresql10-contrib postgresql10-libs
+rm -rf /var/lib/pgsql/10
+rm -rf /usr/pgsql-10
+fi
+
+if [ $pg_version = "11" ]
+then
+yum remove postgresql11 postgresql11-server postgresql11-contrib postgresql11-libs
+rm -rf /var/lib/pgsql/11
+rm -rf /usr/pgsql-11
+fi
