be13cbc11bec796b9cf211c023fa7115db68dd99
dbc2000 <38680220+dbc2000@users.noreply.github.com>
2020-04-16 17:32:46 +0200

    Update install_postgres.sh

--- install_postgres.sh
+++ install_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Veuillez saisir la version de PostgreSQL a installée (9.4/9.5/9.6/9.6/10/11/12) :"
+echo "Enter the PostgreSQL version to be installed: (9.4/9.5/9.6/9.6/10/11/12) :"
 read pg_version
 echo $pg_version
 
